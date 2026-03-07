a49f4a8273d48be7d3948c7327907ce59bafdff7
gablilli <167802854+gablilli@users.noreply.github.com>
2026-03-07 17:54:41 +0100

    fix: py script

--- fix_cgbi.py
+++ fix_cgbi.py
@@ -1,19 +1,19 @@
 import glob
 import pyipng
+import os
 
-# converts weird Apple PNG CgBI format to normal PNGs, thanks amythiz
+DATA_DIR = './data'
 
-def fix_iPNG(file_path):        
+def fix_iPNG(file_path):
     try:
         with open(file_path,'rb') as f:
-            bytes = f.read()
-            fix_bytes = pyipng.convert(bytes)
-            with open(file_path,'wb') as f:
-                f.write(fix_bytes)
-                print(f'{file_path} fixed')
+            raw_bytes = f.read()
+        fix_bytes = pyipng.convert(raw_bytes)
+        with open(file_path,'wb') as f:
+            f.write(fix_bytes)
+            print(f'[FIXED] {file_path}')
     except ValueError:
-        print(f'{file_path} doesn\'t need fixing')
+        print(f'[SKIP] {file_path} doesn\'t need fixing')
 
-
-for file in glob.glob('../data/*/*.png'):
+for file in glob.glob(os.path.join(DATA_DIR, '*', '*.png')):
     fix_iPNG(file)
