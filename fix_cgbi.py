82de46d2dfe0a3f7e4780ab8faced4070fcc9d97
gablilli <167802854+gablilli@users.noreply.github.com>
2026-03-07 17:58:37 +0100

    fix: minor fixes

--- fix_cgbi.py
+++ fix_cgbi.py
@@ -1,19 +1,20 @@
 import glob
-import pyipng
 import os
+from PIL import Image
 
 DATA_DIR = './data'
 
 def fix_iPNG(file_path):
     try:
-        with open(file_path,'rb') as f:
-            raw_bytes = f.read()
-        fix_bytes = pyipng.convert(raw_bytes)
-        with open(file_path,'wb') as f:
-            f.write(fix_bytes)
-            print(f'[FIXED] {file_path}')
-    except ValueError:
-        print(f'[SKIP] {file_path} doesn\'t need fixing')
+        im = Image.open(file_path)
 
-for file in glob.glob(os.path.join(DATA_DIR, '*', '*.png')):
-    fix_iPNG(file)
+        im.save(file_path, format='PNG')
+        print(f'[FIXED] {file_path}')
+
+    except Exception as e:
+        print(f'[ERROR] {file_path}: {e}')
+
+
+if __name__ == "__main__":
+    for file in glob.glob(os.path.join(DATA_DIR, '*', '*.png')):
+        fix_iPNG(file)
